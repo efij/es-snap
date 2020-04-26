621fdd2433df65f19202d197ee49cf3e2ffe751e
shir677 <64060644+shir677@users.noreply.github.com>
2020-04-26 10:28:04 +0300

    fix bugs delete whitespacs

--- snap.py
+++ snap.py
@@ -17,47 +17,48 @@ class SnapshotManager:
                                         http_auth=(usr,password),
                                         verify_certs=verify_certs)
 
-
     def info(self):
         try:
             info = self.connect.info()
+            return info
         except Exception as e:
             raise Exception(str(e))
-        return info
-
-
 
     def add_index(self,index,id,body):
         try:
             index = self.connect.index(index=index,
                                        id=id,
                                        body=body)
-
         except Exception as e:
             raise Exception(str(e))
-
-
-
 
     def delete_index(self,index):
         try:
-            getrepository = self.connect.delete_by_query(index=index,body={"query": {"match_all": {}}})
-
+             self.connect.indices.delete(index=index)
         except Exception as e:
             raise Exception(str(e))
-        return getrepository
 
+    def delete_doc(self,index,id):
+        try:
+             self.connect.delete(index=index,id=id)
+        except Exception as e:
+            raise Exception(str(e))
+
+    def exists_index(self,index):
+        try:
+            se = self.connect.indices.exists(index=index)
+            return se
+        except Exception as e:
+            raise Exception(str(e))
 
     def snapshot_get_repository(self,repository,master_timeout=None,local=None):
         try:
             getrepository = self.connect.snapshot.get_repository(repository=repository,
                                                      local=local,
                                                      master_timeout=master_timeout)
-
+            return getrepository
         except Exception as e:
             raise Exception(str(e))
-        return getrepository
-
 
     def snapshot_clean_repository(self,repository,master_timeout=None,timeout=None):
         try:
@@ -67,9 +68,6 @@ class SnapshotManager:
         except Exception as e:
             raise Exception(str(e))
 
-
-
-
     def snapshot_create_repository(self,repository,repository_body,master_timeout=None):
         try:
             self.connect.snapshot.create_repository(repository=repository,
@@ -78,19 +76,15 @@ class SnapshotManager:
         except Exception as e:
             raise Exception(str(e))
 
-
-
     def get_snapshot(self,repository,snapshot,verbose=True,master_timeout=False):
         try:
             getsnapshot = self.connect.snapshot.get_snapshot(repository=repository,
                                                    snapshot=snapshot,
                                                    master_timeout=master_timeout,
                                                    verbose=verbose)
+            return getsnapshot
         except Exception as e:
             raise Exception(str(e))
-        return getsnapshot
-
-
 
     def create_snapshot(self,repository,snapshot,body,master_timeout=None):
         try:
@@ -100,12 +94,9 @@ class SnapshotManager:
                                          body=body,
                                          master_timeout=master_timeout,
                                          wait_for_completion = True)
-
+            return name
         except Exception as e:
             raise Exception(str(e))
-        return name
-
-
 
     def snapshot_delete(self):
         try:
@@ -115,20 +106,27 @@ class SnapshotManager:
         except Exception as e:
             raise Exception(str(e))
 
-
     def snapshot_restore(self,repository,snapshot,body=None,master_timeout=None):
+        closeindex=[]
         try:
+            openindex= self.connect.snapshot.get(repository=sm["repository_name"], snapshot=snapshot)['snapshots'][0]['indices']
+            for i in openindex:
+                if self.exists_index(i):
+                    self.connect.indices.close(index=i)
+                    closeindex.append(i)
             restore = self.connect.snapshot.restore(repository=repository,
                                                     snapshot=snapshot,
                                                     body=body,
                                                     master_timeout=master_timeout)
+            for i in closeindex:
+                self.connect.indices.open(index=i)
         except Exception as e:
             raise Exception(str(e))
-
 
     def delta_snapshot(self, index_name, start_time, end_time):
         #TODO
         print()
+
 
 
 def parse_configuration(confFile):
@@ -144,14 +142,15 @@ def parse_configuration(confFile):
             conection[ops]=config.get('conection', ops)
     return conection
 
-
 if __name__ == '__main__':
     sm = parse_configuration('snap.ini')
-    a=SnapshotManager(sm["cloud_id"],sm["usr"],sm["password"])
+    z= parse_configuration(r'C:\Users\shiri\Desktop\ss.ini')
+    a=SnapshotManager(z["cloud_id"],z["usr"],z["password"])
 
-    a.add_index(sm["index_name"],int(sm["id"]),json.loads(sm["index_body"]))
-    name = a.create_snapshot(sm["repository_name"],sm["snapshot_prefix"],{"indices":sm["index_name"],"query":{"range" : {"timestamp":{"gte":"2020-04-2302:11:40.633737","lte":"now"}}}})
-    a.delete_index(sm["index_name"])
-    a.snapshot_restore(sm["repository_name"],name,{"indices":sm["index_name"],"include_global_state":True,"rename_pattern":sm["index_name"],"rename_replacement":sm["index_restore_name"]})
 
+
+
+
+'''example for snapshot_restore: a.snapshot_restore(sm["repository_name"],"snap_2020-04-2403:09:57.096488",{"index_settings":{"include_global_state":True}})'''
+'''example for create_snapshot: a.create_snapshot(sm["repository_name"],sm["snapshot_prefix"],{"indices":sm["index_name"]'''
 '''body have to be a dict'''
